65845b555ef7a193f219d1aa952d446662cd4f45
BH04250909 <2182726049@qq.com>
2023-02-18 19:50:08 +0800

    第一周-搭建环境，测试代码-成功

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("大家好！");
+        System.out.println("Nice to meet you");
+        Student stu = new Student();
+        stu.speak("We are students");
+    }
+}
+
+class Student{
+    public  void speak(String s){
+        System.out.println(s);
+    }
+}
